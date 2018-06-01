c8d1cf1cb7542604f165d5e774da4bbd2adcb995
Rafael Franca <Skad14@users.noreply.github.com>
2018-05-31 23:53:28 -0300

    Commit 3
    Commit 3

--- app/src/main/java/com/pjx/skad/myapplication/NewsActivity.kt
+++ app/src/main/java/com/pjx/skad/myapplication/NewsActivity.kt
@@ -1,12 +1,25 @@
 package com.pjx.skad.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.view.ViewPager
+import kotlinx.android.synthetic.main.activity_news.*
 
 class NewsActivity : AppCompatActivity() {
+
+    private lateinit var newsPager: ViewPager
+    var imgPager: IntArray = intArrayOf(R.drawable.iesb_campus_norte, R.drawable.iesb_campus_oeste, R.drawable.iesb_campus_sul)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
+
+        newsButton.setOnClickListener {
+            var profileIntent = Intent(this, ProfileActivity::class.java)
+            startActivity(profileIntent)
+        }
+
+        newsPager = findViewById(R.id.newsPager)
     }
 }
