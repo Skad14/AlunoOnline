fefd3e766d3d038b814315677c768b9d124c457b
Rafael Franca <Skad14@users.noreply.github.com>
2018-06-07 21:09:17 -0300

    Commit 07.06.18
    Login e cadastro funcional

--- app/src/main/java/com/pjx/skad/myapplication/MainActivity.kt
+++ app/src/main/java/com/pjx/skad/myapplication/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.pjx.skad.myapplication
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    private var email: String? = null
+    private var password: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mAuth = FirebaseAuth.getInstance()
+
         loginButton.setOnClickListener {
-            validateLogin()
+            loginUser()
         }
 
         registerText.setOnClickListener {
@@ -26,25 +36,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun validateLogin() {
+    private fun loginUser() {
+        email = inputEmail.text.toString()
+        password = inputPassword.text.toString()
 
-        if (inputEmail.text.isEmpty()) {
-            txtInputEmail.isErrorEnabled = true
-            txtInputEmail.error = getString(R.string.email_blank)
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mAuth!!.signInWithEmailAndPassword(email!!, password!!)
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            updateUI()
+                        } else {
+                            Toast.makeText(this@MainActivity, "Erro na autenticação.",
+                                    Toast.LENGTH_SHORT).show()
+                        }
+                    }
         } else {
-            txtInputEmail.isErrorEnabled = false
+            Toast.makeText(this, "Preencha todos os dados", Toast.LENGTH_SHORT).show()
         }
+    }
 
-        if (inputPassword.text.isEmpty()) {
-            txtInputPassword.isErrorEnabled = true
-            txtInputPassword.error = getString(R.string.password_blank)
-        } else {
-            txtInputPassword.isErrorEnabled = false
-        }
-
-        if(!txtInputEmail.isErrorEnabled && !txtInputPassword.isErrorEnabled) {
-            var newsIntent = Intent(this, NewsActivity::class.java)
-            startActivity(newsIntent)
-        }
+    private fun updateUI() {
+        val intent = Intent(this@MainActivity, NewsActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+        startActivity(intent)
     }
 }
--- app/src/main/java/com/pjx/skad/myapplication/RegisterActivity.kt
+++ app/src/main/java/com/pjx/skad/myapplication/RegisterActivity.kt
@@ -3,45 +3,53 @@ package com.pjx.skad.myapplication
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_register.*
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+    private var email: String? = null
+    private var password: String? = null
+    private var passwordConf: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
+        mAuth = FirebaseAuth.getInstance()
+
         registerButton.setOnClickListener {
-            validateRegister()
+            createNewAccount()
         }
     }
 
-    private fun validateRegister() {
+    private fun createNewAccount() {
+        email = registerEmail.text.toString()
+        password = registerPassword.text.toString()
+        passwordConf = registerConfPassword.text.toString()
 
-        if (registerEmail.text.isEmpty()) {
-            txtRegisterEmail.isErrorEnabled = true
-            txtRegisterEmail.error = getString(R.string.email_blank)
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(passwordConf)) {
+            if (password == passwordConf) {
+                mAuth!!.createUserWithEmailAndPassword(email!!, password!!)
+                        .addOnCompleteListener(this) { task ->
+                            if (task.isSuccessful) {
+                                Toast.makeText(this@RegisterActivity, "Cadastrado com sucesso!", Toast.LENGTH_SHORT).show()
+                                val intent = Intent(this@RegisterActivity, MainActivity::class.java)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                                startActivity(intent)
+                            } else {
+                                Toast.makeText(this@RegisterActivity, "Erro de autenticação.",
+                                        Toast.LENGTH_SHORT).show()
+                            }
+                        }
+            } else {
+                Toast.makeText(this, "As senhas não são iguais", Toast.LENGTH_SHORT).show()
+            }
         } else {
-            txtRegisterEmail.isErrorEnabled = false
-        }
-
-        if (registerPassword.text.isEmpty()) {
-            txtRegisterPassword.isErrorEnabled = true
-            txtRegisterPassword.error = getString(R.string.password_blank)
-        } else {
-            txtRegisterPassword.isErrorEnabled = false
-        }
-
-        if (registerConfPassword.text.isEmpty()) {
-            txtRegisterConfPassword.isErrorEnabled = true
-            txtRegisterConfPassword.error = getString(R.string.password_conf_blank)
-        } else {
-            txtRegisterConfPassword.isErrorEnabled = false
-        }
-
-        if(!txtRegisterEmail.isErrorEnabled && !txtRegisterPassword.isErrorEnabled && !txtRegisterConfPassword.isErrorEnabled) {
-            var mainIntent = Intent(this, MainActivity::class.java)
-            startActivity(mainIntent)
+            Toast.makeText(this, "Preencha todos os dados", Toast.LENGTH_SHORT).show()
         }
     }
 }
